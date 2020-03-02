bb8f9e3d5ead61d522d81bd6167a53cce42d35df
Victor Aprea <victor.aprea@wickeddevice.com>
2020-03-02 12:27:39 -0500

    routing problem

--- libraries/academy-lib/src/lib/academy-lib.module.ts
+++ libraries/academy-lib/src/lib/academy-lib.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { AcademyLibComponent } from './academy-lib.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: AcademyLibComponent
+  }
+];
 
 @NgModule({
   declarations: [AcademyLibComponent],
   imports: [
+    RouterModule.forChild(routes)
   ],
-  exports: [AcademyLibComponent]
+  exports: [RouterModule]
 })
 export class AcademyLibModule { }
